356496fbc96a3e09af4f9879b988457b54e5749f
Darjan Divkovic <daarjan.tz@gmail.com>
2019-08-12 13:54:29 +0200

    Created Person Class

--- src/main/java/SchoolManagementSystem/com/darjanDivkovic/Person.java
+++ src/main/java/SchoolManagementSystem/com/darjanDivkovic/Person.java
@@ -13,6 +13,20 @@ package SchoolManagementSystem.com.darjanDivkovic;
  */
 public class Person {
 	
+	// Data fields
+	private int id;
+	private String name;
+	private String surname;
+	
+	// Constructors
+	Person(int id, String name, String surname)
+	{
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}
+	
+	
 	
 
 }
