ec4ab52b18be65c905366b4f9ef80965dbbe8723
Darjan Divkovic <daarjan.tz@gmail.com>
2019-08-12 13:49:24 +0200

    Initial commit!

---
+++ src/main/java/SchoolManagementSystem/com/darjanDivkovic/Person.java
@@ -0,0 +1,18 @@
+package SchoolManagementSystem.com.darjanDivkovic;
+
+/*
+ * Purpose of this class is to be extended by :
+ * 	- Student
+ *  - &
+ *  - Teacher Class
+ *  
+ * Should contain info like:
+ * 	- id
+ * 	- name
+ *  - ...
+ */
+public class Person {
+	
+	
+
+}
