736a84a2e6036f5b8389f544f0ec254de80bf416
Darjan Divkovic <daarjan.tz@gmail.com>
2019-08-12 18:29:39 +0200

    ADDED: Plans for the future of the project!

--- src/main/java/SchoolManagementSystem/com/darjanDivkovic/MAIN.java
+++ src/main/java/SchoolManagementSystem/com/darjanDivkovic/MAIN.java
@@ -1,6 +1,19 @@
 package SchoolManagementSystem.com.darjanDivkovic;
 
 public class MAIN {
+	
+	/*
+	 * ZA URADITI:
+	 * - NAPRAVITI UI - OMOGUCITI : PRAVLJENJE LJUDI, PLACANJE STUDENATA, ISPLACIVANJE PLACA UCITELJIMA
+	 * - POVEZATI SA BAZOM
+	 * - NAPRAVITI LOG 
+	 * - NAPRAVITI GUI
+	 * - NAPRAVITI SERVLET OD OVOGA
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 
 	public static void main(String[] args) {
 		
